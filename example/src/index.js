59a7dd607721ed4c60788a938ac2c687d81022ac
Cássio Paixão <cassiopaixaoti@gmail.com>
2019-05-05 14:21:46 -0300

    create babel-preset-node-app

--- example/src/index.js
+++ example/src/index.js
@@ -5,12 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', async (req, res) => {
-  try {
-    const thing = await Promise.resolve({ one: 'two' });
-    return res.json({ ...thing, hello: 'world' });
-  } catch (e) {
-    return res.json({ error: e.message });
-  }
+  res.json({ message: 'hello world' });
 });
 const port = process.env.PORT || 3000;
 
@@ -18,10 +13,5 @@ app.listen(port, err => {
   if (err) {
     console.error(err);
   }
-
-  if (__DEV__) {
-    console.log('> in developmen');
-  }
-
   console.log(`> listening on port ${port}`);
 });
